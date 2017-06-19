01317e3049ebc5dbfcd23c9d4779a4ac005068a0
pedro <pedrohlsoares@hotmail.com>
2017-06-19 11:15:11 -0300

    Second Commit

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,4 @@
-$.get("/reader.php?action=scan", function(data, status){
-    var songs = JSON.parse(data);
-    console.log(songs);
-});
+// $.get("/reader.php?action=scan", function(data, status){
+//     var songs = JSON.parse(data);
+//     console.log(songs);
+// });
